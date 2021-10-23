208047c5f33175a4909f3e6ed8419477e8f88232
Vedanth393 <87324148+Vedanth393@users.noreply.github.com>
2021-10-23 14:22:23 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var sasuke,sasukeImg
+var narutoImg,naruto
+var swamp,swampImg
+
+function preload(){
+narutoImg=loadImage("Naruto.jpg")
+sasukeImg=loadImage("Sasuke.png")
+swampImg=loadImage("swamp.png")
+}
+
+
+
+function setup() {
+ createCanvas(windowWidth,windowHeight)
+
+ swamp=createSprite(300,300)
+ swamp.addImage(swampImg)
+ swamp.scale=3
+
+
+sasuke=createSprite(width/2,height-300,30,30) 
+sasuke.addImage("running",sasukeImg)
+sasuke.velocityX=3;
+sasuke.scale=.7
+
+}
+
+function draw() {
+    background("white");
+    sasuke.y= World.mouseY;
+    drawSprites();
+    
+createNaruto()
+}
+
+function createNaruto(){
+    if(World.frameCount % 200 == 0){
+    var naruto=createSprite(Math.round(random(height-50,),40,10,10))
+    naruto.addImage("running",narutoImg)
+    naruto.scale=0.7
+    naruto.velocityX = 3
+    }
+}
+
+
+
+
